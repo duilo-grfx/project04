3e4da361d2c050f28b4d9fc2e013e21973cdd758
duilo-grfx <79026629+duilo-grfx@users.noreply.github.com>
2021-05-07 17:18:54 -0400

    updates

---
+++ includes/html-bottom.php
@@ -0,0 +1,21 @@
+<script>
+// Tabbed Menu
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+}
+document.getElementsByClassName("myLink").click();
+</script>
+
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
---
+++ includes/nav.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="file1.php">Giza</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="file2.php">San Francisco</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="file3.php">Yangzhou</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="page.php">Random Page</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
